a78ffefd3168212a9be4fc718a94053c0d47359a
atikurahad <atikurahad@gmail.com>
2025-04-16 14:21:58 +0600

    variable in go lang

--- main.go
+++ main.go
@@ -4,6 +4,34 @@ import "fmt"
 
 func main (){
 
-fmt.Println("Hi, Welcome to Go lang")
-fmt.Println("Hi, Are you okay?")
+
+	/*
+	Variable Naming Convention 
+  
+	1. letter,digit,_.
+	2. keywords are not allowed as variable.
+	3. variable can not have space.
+	4. variable name can not start with digit. 
+	*/
+
+	
+	// Variable Declaration Convention 
+  
+	 var fullName  , country string;
+	var age  int;
+	var cgpa float32;
+	
+	
+
+	//Variable Initialization Convention 
+  
+	fullName = "Atikur Rahman"
+	 country = "Bangladesh"
+	 age = 25
+	 cgpa = 3.15
+	
+
+fmt.Println(fullName , "i am" ,age, "years old")
+fmt.Println("I live in", country)
+fmt.Println("My cgpa is " ,cgpa,)
 }
